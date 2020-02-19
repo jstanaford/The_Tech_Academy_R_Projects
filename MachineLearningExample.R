0f5786d315c8262cff9a85c679b2df48c3ddb28f
s42destroyer <53664408+s42destroyer@users.noreply.github.com>
2020-02-18 17:36:50 -0800

    Upload 2

---
+++ MachineLearningExample.R
@@ -0,0 +1,85 @@
+# Predicting with Machine Learning 
+
+# Load the data 
+data(iris)
+
+# Set a seed to make randomness reproducable
+set.seed(42)
+
+# Randomly sampl 100 of 150 row indexes
+indexes <- sample(
+  x = 1:150,
+  size = 100)
+
+# Inspect the random indexes
+print(indexes)
+
+# Create a training set from indexes
+train <- iris[indexes, ]
+
+# Create a test set from remaining indexes
+test <- iris[-indexes, ]
+
+# Load the decision tree package
+library(tree)
+
+# Train a decision tree model
+model <- tree(
+  formula = Species ~ .,
+  data = train
+)
+
+# Inspect the model
+summary(model)
+
+# Visualize the decision tree model
+plot(model)
+text(model)
+
+# Load color brewer library
+library(RColorBrewer)
+
+# Create a color palette
+palette <- brewer.pal(3, "Set2")
+
+# Create a scatterplot colored by species
+plot(
+  x = iris$Petal.Length,
+  y = iris$Petal.Width,
+  pch = 19,
+  col = palette[as.numeric(iris$Species)],
+  main = "Iris Petal Length vs. Width", 
+  xlab = "Petal Length (cm)",
+  ylab = "Petal Width (cm)"
+)
+
+# Plot the decision boundaries
+partition.tree(
+  tree = model,
+  label = "Species",
+  add = TRUE
+)
+
+# Predict with the model
+predictions <- predict(
+  object = model,
+  newdata = test,
+  type = "class"
+)
+
+# Create a confusion matrix
+table(
+  x = predictions,
+  y = test$Species
+)
+
+# Load the caret Package
+library(caret)
+
+# Evaluate the prediction results
+confusionMatrix(
+  data = predictions,
+  reference = test$Species
+)
+
+save(model, file = "Tree.RData")
